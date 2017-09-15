50a911a930d8d1145c7afbd3533fa5d7a1620c5c
Korisnik <vrafaeli@msn.com>
2017-09-16 00:03:29 +0200

    New background test

--- src/setup/createScene.js
+++ src/setup/createScene.js
@@ -4,7 +4,7 @@ const loader = new CubeTextureLoader();
 loader.setPath();
     
 const texture = loader.load( [
-    'space7.png', 'space7.png', 'space7.png', 'space7.png', 'space7.png', 'space7.png'
+    'space10.png', 'space10.png', 'space10.png', 'space10.png', 'space10.png', 'space10.png'
 ] ); 
 
 
