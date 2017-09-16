809ce491080e256e6a60dd619b8f213bb15cd616
Korisnik <vrafaeli@msn.com>
2017-09-16 12:12:58 +0200

    minor

--- src/dashboard.js
+++ src/dashboard.js
@@ -50,6 +50,6 @@ gameDescription.setAttribute('class', 'gameDescription');
 gameDescription.innerHTML=
     'Use A,S,D,W to move. Left click to fire. Use CTRL+fire to fire rocket.<br />' +
     'Rocket has cooldown, shown bottom right. Right click the objects to get their info.<br />' +
-    'Use shift key to enter cruise mode. Cruise engine should load first (check bottom left corner with loading info).<br />' + 
-    'OBJECTIVE: Destroy targets! (when done it will show win message).';
+    'Use shift key to enter cruise mode. Cruise engine has to load first (check bottom left corner with loading info).<br />' + 
+    'OBJECTIVE: Destroy targets! (2 targets in this version)';
 container.appendChild( gameDescription );
