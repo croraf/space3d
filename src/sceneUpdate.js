5f00b78b849cbe641cbce4c97766141099c5a82d
Korisnik <vrafaeli@msn.com>
2017-09-16 15:24:45 +0200

    minor

--- src/sceneUpdate.js
+++ src/sceneUpdate.js
@@ -37,7 +37,7 @@ const sceneUpdate = () => {
 
 
     rotateAroundWorldAxis(scene.getObjectByName('target1'), new Vector3(0,0,-200), new Vector3(0,1,0), 0.001);
-    rotateAroundWorldAxis(scene.getObjectByName('target2'), new Vector3(100,0,100), new Vector3(0,1,0), 0.0014);
+    rotateAroundWorldAxis(scene.getObjectByName('target2'), new Vector3(100,0,100), new Vector3(0,1,0), 0.0015);
     rotateAroundWorldAxis(scene.getObjectByName('target3'), new Vector3(100,0,-300), new Vector3(0,1,0), 0.001);
     rotateAroundWorldAxis(scene.getObjectByName('target4'), new Vector3(0,0,-180), new Vector3(0,1,0), 0.0015);
 };
